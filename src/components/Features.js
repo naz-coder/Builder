61a2c6e421001144b450a1080c0b1a3256bc45b9
naz-coder <favouritenaz@gmail.com>
2022-02-22 23:34:10 +0100

    Features

--- src/components/Features.js
+++ src/components/Features.js
@@ -1,49 +1,52 @@
-import React from 'react'
-// import MdGroups from 'react-icons/fa'
-// import FaChartLine from 'react-icons/fa'
-// import FaFolderPlus from 'react-icons/fa'
-// import FaChartPie from 'react-icons/fa'
+import StakingIcon from '../images/StakingIcon.png'
+import ReferaIcon from '../images/ReferalIcon.png'
+import Daapp from '../images/Daapp.png'
+import WeeklyIcon from '../images/WeeklyIcon.png'
+import AmbIcon from '../images/AmbIcon.png'
+import TranspyIcon from '../images/TranspyIcon.png'
 
 const Features = () => {
   return (
-    <div className='Feature'>
+    <div className='feature'>
         <h1>Features</h1>
         <p>There are six main features Builder developed to 
-          deliver a defi ecosystem that builds value</p>
+          deliver a defi ecosystem that builds value.</p>
         <div className='feature-box'>
             <div>
-                {/* <Fa/> */}
+            <img src={StakingIcon} alt='img'/>
                 <h3>Staking Rewards</h3>
             </div>
             <div>
-                {/* <MdGroups/> */}
+            <img src={ReferaIcon} alt='img'/>
                 <h3>Community Referrals</h3>
             </div>
             <div>
-                {/* <FaChartPie/> */}
+                <img src={Daapp} alt='img'/>
+
                 <h3>DApp Rewards</h3>
             </div>
             <div>
-                {/* <FaChartLine/> */}
+            <img src={WeeklyIcon} alt='img'/>
                 <h3>5% Weekly Rewards Target</h3>
             </div>
             <div>
-                {/* <Fa/> */}
+            <img src={AmbIcon} alt='img'/>
                 <h3>4% Ambassador Rewards</h3>
             </div>
             <div>
-                {/* <FaFolderPlus/> */}
+            <img src={TranspyIcon} alt='img'/>
                 <h3>Full Transparency</h3>
             </div>
-            <p>Builder is not only a new web3 solution for businesses, 
+        </div>
+        <h4>Builder is not only a new web3 solution for businesses, 
                 it is a new economic model that is built with the sole 
                 intention of delivering inherent value through the fair 
                 distribution of value to the Builder network. We're utilizing 
                 advanced distributed financial technology to build an incentivized 
                 community. When businesses build on the Builder network, they're not 
                 only building a more efficient and secure solution for themselves but 
-                also can deliver value to all $BLDR stakers.  </p>
-        </div>
+                also can deliver value to all $BLDR stakers.  </h4>
+
     </div>
   )
 }
