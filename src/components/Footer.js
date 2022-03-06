64f5535618a588b06fde1553aaef70c7e163ddc7
naz-coder <favouritenaz@yahoo.com>
2022-03-06 06:59:09 +0100

    Footer added

---
+++ src/components/Footer.js
@@ -0,0 +1,18 @@
+
+const Footer = () => {
+  return (
+    <div className='footer'>
+        <div className="footer-grid">
+            <div>
+                <p>Terms & Conditions</p>
+                <p>Privacy Policy</p>
+            </div>
+            <div>
+                <p>&copy; 2022 - All rights reserved.</p>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Footer
