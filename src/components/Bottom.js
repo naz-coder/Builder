e8b1e6ae1a7a636eabfab68930ac6dbfd7b2511e
naz-coder <favouritenaz@yahoo.com>
2022-03-06 06:45:22 +0100

    Bottom

---
+++ src/components/Bottom.js
@@ -0,0 +1,19 @@
+import {FaLongArrowAltRight} from 'react-icons/fa'
+
+const Bottom = () => {
+  return (
+    <div>
+      <div className="bottom-container">
+        <div className='bottom-grid'>
+        <div>
+            <h3>LETS BUILD THE FUTURE OF DEFI TOGETHER</h3>
+            <h1>Start earning Builder Rewards!</h1>
+        </div>
+        <button>Enter Builder<FaLongArrowAltRight className='icon'/></button>
+        </div>
+    </div>
+    </div>
+  )
+}
+
+export default Bottom
